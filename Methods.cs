6e08bedb79330f3e1291cac64b7cf82689b7b7b1
Pedro Javier Siqueiros López <javiersiqueiros_me@outlook.com>
2022-07-14 17:38:34 -0500

    First commit
    First steps in machine learning.
    Normalization and encoding

---
+++ Methods.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    public static class Methods
+    {
+        public static void GaussNormal(double[][] data, int column)
+        {
+            // sum of terms
+            double sum = 0.0;
+            for (int i = 0; i < data.Length; i++)
+                sum += data[i][column];
+            double mean = sum / data.Length;
+
+            // sum of sqaures
+            double sumSquares = 0.0;
+            for (int i = 0; i < data.Length; i++)
+                sumSquares += Math.Pow((data[i][column] - mean), 2);
+
+            // standar deviation
+            double standarDeviation = Math.Sqrt(sumSquares / data.Length);
+            for (int i = 0; i < data.Length; i++)
+                data[i][column] = (data[i][column] - mean) / standarDeviation;
+        }
+
+        public static void MinMaxNormal(double[][] data, int column)
+        {
+            double min = data[0][column];
+            double max = data[0][column];
+
+            //sorting values
+            for (int row = 0; row < data.Length; row++)
+            {
+                if(data[row][column] < min)
+                    min = data[row][column];
+                if(data[row][column] > max)
+                    max = data[row][column];
+            }
+
+            double range = max - min;
+            if(range == 0.0)
+            {
+                for (int row = 0; row < data.Length; row++)
+                    data[row][column] = 0.5;
+                return;
+            }
+
+            for(int row = 0; row < data.Length; row++)
+                data[row][column] = (data[row][column] - min) / range;
+        }
+
+        public static void ShowMatrix(double[][] matrix, int decimals)
+        {
+            for(int row = 0; row < matrix.Length; row++)
+            {
+                for(int column = 0; column < matrix[row].Length; column++)
+                {
+                    double value = Math.Abs(matrix[row][column]);
+                    if (matrix[row][column] >= 0.0)
+                        Console.Write(" ");
+                    else
+                        Console.Write("-");
+                    Console.Write(value.ToString("F" + decimals).PadRight(5) + " ");
+                }
+                Console.Write(string.Empty);
+            }
+        }
+
+        public static void ShowData(string[] rawData)
+        {
+            for(int i = 0; i < rawData.Length; i++)
+            {
+                Console.WriteLine(rawData[i]);
+            }
+            Console.WriteLine(string.Empty);
+        }
+
+        
+    }
+}
---
+++ PerceptronProgram.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    public class PerceptronProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin data encoding and data normalization demon.\n");
+
+            string[] sourceData = new string[]
+            {
+                " Sex.      Age.     Locale.      Income.      Poltics."  ,
+                "==============================================",
+                "Male       25      Rural       63,000      Conservative"  ,
+                "Female     36      Suburban    55,000      Liberal"  ,
+                "Male       40      Urban       74,000      Moderate"  ,
+                "Female     23      Rural       28,000      Liberal"
+            };
+
+            Console.WriteLine("Dummy data in raw form:\n");
+            Methods.ShowData(sourceData);
+
+            string[] encodedData = new string[]
+            {
+                "-1 25  1  0 63,000 1 0 0",
+                " 1 36  0  1 55,000 0 1 0",
+                "-1 40 -1 -1 74,000 0 0 1",
+                " 1 23  1  0 28,000 0 1 0",
+            };
+
+            Console.WriteLine("\nData after categorical encoding:\n");
+            Methods.ShowData(encodedData);
+
+            Console.WriteLine("\nNumeric data stored in matrix:\n");
+
+            double[][] numericData = new double[4][];
+
+            numericData[0] = new double[] { -1, 25.0,  1,  0, 63000.00, 1, 0, 0 };
+            numericData[1] = new double[] { 1, 36.0,  0,  1, 55000.00, 0, 1, 0 };
+            numericData[2] = new double[] { -1, 40.0, -1, -1, 74000.00, 0, 0, 1 };
+            numericData[3] = new double[] { 1, 23.0,  1,  0, 28000.00, 0, 1, 0 };
+
+            Methods.ShowMatrix(numericData, 2);
+
+            Methods.GaussNormal(numericData, 1);
+            Methods.MinMaxNormal(numericData, 4);
+
+            Console.WriteLine("\nMatrix after normalization Guassian col. 1 and MixMax col. 4\n");
+            Methods.ShowMatrix(numericData, 2);
+
+            Console.WriteLine("\nEnd data encoding and normalization demon.");
+
+            Console.ReadKey();
+        }
+    }
+}
